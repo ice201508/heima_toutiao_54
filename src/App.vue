ea22b39cb0031fef3f6c5698a826d07cc735cf1a
ice19 <2235907780@qq.com>
2020-11-02 02:14:24 +0800

    登录页面基本布局

--- src/App.vue
+++ src/App.vue
@@ -1,22 +1,7 @@
 <template>
   <div id="app">
-    <h2>根组件App <i class="iconfont iconwuwangluo"></i> <i class="iconfont iconyuedu"></i></h2>
-    <div>
-      <van-button type="primary">主要按钮</van-button>
-      <van-button type="info">信息按钮</van-button>
-      <van-button type="default">默认按钮</van-button>
-      <van-button type="warning">警告按钮</van-button>
-      <van-button type="danger">危险按钮</van-button>
-    </div>
-    <div class="box"></div>
     <router-view />
   </div>
 </template>
 
-<style lang="less">
-.box {
-  width: 200px;
-  height: 100px;
-  background-color: skyblue;
-}
-</style>
+<style lang="less"></style>
---
+++ src/views/login.vue
@@ -0,0 +1,57 @@
+<template>
+  <div>
+    <van-nav-bar title="登录">
+      <template #left>
+        <van-icon name="cross" size="18" />
+      </template>
+    </van-nav-bar>
+    <!-- 登录表单 -->
+    <van-form @submit="onSubmit">
+      <van-field
+        v-model="username"
+        name="用户名"
+        label="用户名"
+        placeholder="用户名"
+        :rules="[{ required: true, message: '请填写用户名' }]"
+      >
+        <i slot="left-icon" class="iconfont iconwode"></i>
+      </van-field>
+      <van-field
+        v-model="password"
+        type="password"
+        name="密码"
+        label="密码"
+        left-icon="iconfont iconyanzhengma"
+        placeholder="密码"
+        :rules="[{ required: true, message: '请填写密码' }]"
+      >
+        <i slot="left-icon" class="iconfont iconyanzhengma"></i>
+        <template #button>
+          <van-button size="small" type="primary">发送验证码</van-button>
+        </template>
+      </van-field>
+      <div style="margin: 16px;">
+        <van-button block type="info" disabled native-type="submit">
+          登录
+        </van-button>
+      </div>
+    </van-form>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Login',
+  data() {
+    return {
+      username: '',
+      password: '',
+    };
+  },
+  methods: {
+    onSubmit() {},
+  },
+};
+</script>
+
+<style lang="less" scoped></style>
