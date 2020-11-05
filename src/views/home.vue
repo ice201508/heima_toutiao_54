3d8c3c470a0351407e965316dfcf3d651500ba24
ice19 <2235907780@qq.com>
2020-11-05 08:20:57 +0800

    a

--- src/views/home.vue
+++ src/views/home.vue
@@ -7,9 +7,33 @@
         <div class="text">登录/注册</div>
       </div>
     </div>
-    <div class="box-1" v-else>
-      <div class="login-box" @click="goLogin">
-        123
+    <div class="box-4" v-else>
+      <div class="b4-top">
+        <div class="b4t-left">
+          <img src="@/assets/mobile.png" />
+          <span>我的姓名</span>
+        </div>
+        <div class="b4t-right">
+          <van-button type="default" round size="mini">编辑资料</van-button>
+        </div>
+      </div>
+      <div class="b4-bottom">
+        <div class="item">
+          <div>64422</div>
+          <div>头条</div>
+        </div>
+        <div class="item">
+          <div>64422</div>
+          <div>头条</div>
+        </div>
+        <div class="item">
+          <div>64422</div>
+          <div>头条</div>
+        </div>
+        <div class="item">
+          <div>64422</div>
+          <div>头条</div>
+        </div>
       </div>
     </div>
     <div class="box-2">
@@ -39,6 +63,9 @@
         <van-cell title="单元格" is-link />
       </van-cell-group>
     </div>
+    <div class="logout" v-if="token">
+      <van-button type="primary" block @click="logout">退出登录</van-button>
+    </div>
   </div>
 </template>
 
@@ -49,6 +76,10 @@ export default {
   name: 'Home',
   methods: {
     goLogin() {
+      this.$router.push('/login');
+    },
+    logout() {
+      this.$store.commit('setItem', '');
       this.$router.push('/login');
     },
   },
@@ -85,7 +116,58 @@ export default {
     }
   }
   .box-3 {
-    margin-top: 15px;
+    margin-top: 24px;
+  }
+  .box-4 {
+    height: 360px;
+    background: url('../assets/banner.png');
+    background-size: cover;
+    .b4-top {
+      height: 150px;
+      display: flex;
+      flex-direction: row;
+      padding: 60px 40px 30px;
+      justify-content: space-between;
+      align-items: center;
+      .b4t-left {
+        display: flex;
+        align-items: center;
+        img {
+          width: 100px;
+        }
+        span {
+          margin-left: 20px;
+          color: #fff;
+        }
+      }
+      .b4t-right {
+        .van-button {
+          padding: 0 18px;
+        }
+      }
+    }
+    .b4-bottom {
+      display: flex;
+      justify-content: space-between;
+      align-items: center;
+      padding: 0 40px;
+      text-align: center;
+      color: #fff;
+      .item > div:first-child {
+        font-size: 38px;
+      }
+      .item > div:last-child {
+        font-size: 28px;
+      }
+    }
+  }
+  .logout {
+    margin-top: 24px;
+    .van-button {
+      background-color: #fff;
+      border: none;
+      color: #ee0a24;
+    }
   }
 }
 </style>
