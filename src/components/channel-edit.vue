336e3ce7dfe6f62573f7c08ff5ea875eef5d3758
ice19 <2235907780@qq.com>
2020-11-08 00:03:50 +0800

    基本布局

--- src/components/channel-edit.vue
+++ src/components/channel-edit.vue
@@ -1,10 +1,18 @@
 <template>
   <div class="channel-edit">
+    <!-- 我的频道 -->
+    <van-cell title="我的频道">
+      <template #default>
+        <van-button class="edit-btn" size="mini" type="danger" round plain>编辑</van-button>
+      </template>
+    </van-cell>
     <van-grid :gutter="10">
-      <van-grid-item icon="photo-o" text="文字" />
-      <van-grid-item icon="photo-o" text="文字" />
-      <van-grid-item icon="photo-o" text="文字" />
-      <van-grid-item icon="photo-o" text="文字" />
+      <van-grid-item v-for="i in 13" :key="i" text="文字" />
+    </van-grid>
+    <!-- 推荐频道 -->
+    <van-cell title="推荐频道"></van-cell>
+    <van-grid :gutter="10">
+      <van-grid-item v-for="i in 13" :key="i" text="文字" />
     </van-grid>
   </div>
 </template>
@@ -18,5 +26,11 @@ export default {
 <style lang="less" scoped>
 .channel-edit {
   padding-top: 100px;
+  .van-cell::after {
+    border-bottom: none;
+  }
+  .edit-btn {
+    padding: 0 20px;
+  }
 }
 </style>
