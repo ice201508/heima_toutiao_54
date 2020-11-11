7432c3d5eb1c8e019cbbd2d0601d2104f1464cf2
ice19 <2235907780@qq.com>
2020-11-12 00:28:31 +0800

    发表评论基本组件

--- src/components/article/article-comment.vue
+++ src/components/article/article-comment.vue
@@ -1,8 +1,18 @@
 <template>
   <div class="article-comment">
-    <div class="item">
-      <div class="left">
-        <van-image round fit="cover" src="https://img.yzcdn.cn/vant/cat.jpeg" />
+    <div class="item clearfix" v-for="item in 7" :key="item">
+      <van-image class="one" round fit="cover" src="https://img.yzcdn.cn/vant/cat.jpeg" />
+      <div class="two">
+        <div class="t-title">张三</div>
+        <div class="t-content">评论正文内容部分</div>
+        <div class="time">
+          <span>三天前</span>
+          <van-button type="default" round>回复0</van-button>
+        </div>
+      </div>
+      <div class="three">
+        <van-icon name="good-job-o" />
+        <span>赞</span>
       </div>
     </div>
   </div>
@@ -17,9 +27,46 @@ export default {
 <style lang="less" scoped>
 .article-comment {
   .item {
-    .van-image {
+    padding-bottom: 30px;
+    margin-bottom: 30px;
+    border-bottom: 1px solid #ebe5e5;
+    .one.van-image {
       width: 72px;
       height: 72px;
+      float: left;
+    }
+    .two {
+      float: left;
+      margin-left: 20px;
+      font-size: 24px;
+      .t-title {
+        color: cornflowerblue;
+        font-size: 32px;
+      }
+      .t-content {
+        font-size: 28px;
+        margin: 30px 0;
+        color: #000;
+      }
+      .time {
+        .van-button {
+          margin-left: 20px;
+          font-size: 24px;
+          height: 44px;
+        }
+      }
+    }
+    .three {
+      float: right;
+      font-size: 30px;
+      .van-icon {
+        font-size: 36px;
+      }
+      span {
+        margin-left: 8px;
+        position: relative;
+        top: -6px;
+      }
     }
   }
 }
